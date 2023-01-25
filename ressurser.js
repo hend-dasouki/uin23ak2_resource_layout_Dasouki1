e837d006f47442633215373e4a6bdc8e9bd13bf7
hend-dasouki <111729605+hend-dasouki@users.noreply.github.com>
2023-01-25 15:59:56 +0100

    Add files via upload

---
+++ ressurser.js
@@ -0,0 +1,138 @@
+const resources = [
+    {
+        category: "Kategori 1",
+        h2:"HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "Kategori 2",
+        h2:"CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "Kategori 3",
+        h2:"JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Kategori 4",
+        h2:"React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Kategori 5",
+        h2:"Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+const menu = document.getElementById("menu")
+let menuHTML= ``;
+resources.map((category) => {
+        
+
+    let subMenu = ``;
+    category.sources.forEach((item) => {
+      subMenu += `
+    <li><a target="_blank" href=${item.url}>${item.title}</a></li>
+   ` ;
+    }); 
+
+    menuHTML += `
+
+  <seaction class="mainMenuItem">
+      <button>${category.category}</button>
+      <div class="open">
+      <h2 class="h2">${category.h2}</h2>
+      <p class="text">${category.text}</p>
+      <ul class="subMenu">
+            ${subMenu}
+      </ul>
+      </div>
+  </seaction>
+`;
+  })
+
+
+  menu.innerHTML = `
+<ul class="mainMenu">
+    ${menuHTML}
+</ul>
+`;
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+//Løsning var at jeg brukt map.() for å hente categoriene  også brukte buttom for å åpne  listen og printe ut ul lista 
